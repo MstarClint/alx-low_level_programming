7a26707c49eacd1cf5663da06fe66defea672328
MstarClint <obiohaclinton111@gmail.com>
2023-10-10 05:44:38 +0100

    Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#inlclude<studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        puts("with proper grammer, but the outcome is a piece of art,\n");
+        return (0);
+}
