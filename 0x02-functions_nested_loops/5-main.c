d5ece87e3bcc88ec1f8155cce2aae9bbd84b41da
MstarClint <obiohaclinton111@gmail.com>
2023-10-11 12:46:34 -0700

    5-sign

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,33 +1,25 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
+ * Return: 1 for positive, -1 for negative, 0 for zero
  */
-int main(void)
+int print_sign(int n)
 {
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (n > 0)
+	{
+		_putchar(43); /* 43 is ASCII code for '+' */
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45); /* 45 is ASCII code for '-' */
+		return (-1);
+	}
+	else
+	{
+		_putchar(48); /* 48 is ASCII code for '0' */
+		return (0);
+	}
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,48 +1,34 @@
 #include "main.h"
 
-
 /**
-
- * print_sign - Printys sum
-
- * @n: The nmber
-
- * Return: 1 for all
-
- */
-
-int print_sign(int n)
-
+ * main - Entry point to check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
+	int r;
 
-	if (n > 0)
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
 
-	{
-
-		_putchar(43);
-
-		return (1);
-
-	}
-
-	else if (n < 0)
-
-	{
-
-		_putchar(45);
-
-		return (-1);
-
-	}
-
-	else
-
-	{
-
-		_putchar(48);
-
-		return (0);
-
-	}
-
+	return (0);
 }
